0f01ed6fe9442102e911030517427f1683ac9e75
Shubham Tyagi <styagi.2301@gmail.com>
2019-05-17 21:14:46 +0530

    Major Update
    Added UI
    Added GPS Plugin

---
+++ Assets/_MyStuff/Scripts/GoogleServicesManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class GoogleServicesManager : MonoBehaviour
+{
+    //public Button openAchievements;
+    //public Button openLeaderboard;
+    //public Button connectToGS;
+
+    private bool isConnectedToGoogleServices;
+    private UIManager uiManager;
+
+    private void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+    void Start()
+    {
+        isConnectedToGoogleServices = false;
+
+        Init();
+
+    }
+
+    private void Init()
+    {
+        PlayGamesPlatform.Activate();
+    }
+
+    public void ConnectToGoogleServices()
+    {
+        string msg = "";
+
+        if (isConnectedToGoogleServices)
+        {
+            msg = "Already Connected";
+            if (uiManager != null)
+                uiManager.ShowToast(msg);
+            return;
+        }
+
+        Social.localUser.Authenticate((bool succes) =>
+        {
+            isConnectedToGoogleServices = succes;
+        });
+
+        if (!isConnectedToGoogleServices) msg = "Unable to connect";
+        else msg = "Connected";
+
+        if (uiManager != null)
+            uiManager.ShowToast(msg);
+
+    }
+
+    public void OpenAchievements()
+    {
+        string msg = "";
+
+        if (Social.localUser.authenticated)
+            Social.ShowAchievementsUI();
+        else
+        {
+            msg = "Unable to open Achievements";
+        }
+
+        if (uiManager != null)
+            uiManager.ShowToast(msg);
+
+    }
+
+    public void OpenLeaderboard()
+    {
+        string msg = "";
+
+        if (Social.localUser.authenticated)
+            Social.ShowLeaderboardUI();
+        else
+        {
+            msg = "Unable to open Leaderboard";
+
+        }
+
+        if (uiManager != null)
+            uiManager.ShowToast(msg);
+
+    }
+}
---
+++ Assets/_MyStuff/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject toastParent;
+    public Text toastText;
+
+    private Vector3 toastParentLerpPosition;
+
+    void Start()
+    {
+        toastParentLerpPosition = toastParent.transform.localPosition;
+    }
+
+    private void Update()
+    {
+        toastParent.transform.localPosition = Vector3.Lerp(toastParent.transform.localPosition, toastParentLerpPosition, 0.2f);
+    }
+
+    public void ShowToast(string _text)
+    {
+        toastText.text = _text;
+        toastParentLerpPosition = new Vector3(000, 50, 0);
+        Invoke("HideToast", 2f);
+    }
+
+    public void HideToast()
+    {
+        toastParentLerpPosition = new Vector3(000, -150, 0);
+    }
+}
